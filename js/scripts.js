5b02f592da977192ed4277da6457f89ed8bc4544
Danny Hartnell <danielhartnell+epicodus@gmail.com>
2015-10-24 11:29:39 -0700

    add initial HTML layout with bootstrap CSS references.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  var goal = prompt("What number would you like to ping-pong up to?");
+  var currentValue = 1;
+
+  while (currentValue <= goal) {
+    if (((currentValue % 3) === 0) && ((currentValue % 5) === 0)) {
+      $(".dynamic-list").append("<li>ping-pong</li>");
+    } else if ((currentValue % 3) === 0) {
+      $(".dynamic-list").append("<li>ping</li>");
+    } else if ((currentValue % 5) === 0) {
+      $(".dynamic-list").append("<li>pong</li>");
+    } else {
+      $(".dynamic-list").append("<li>" + currentValue + "</li>");
+    };
+    currentValue++;
+  };
+});
